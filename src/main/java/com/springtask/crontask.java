df34174c072fa2d2d8ef2ac4d6a819d68739d704
WeeeiiiiiiCoder <chouwe052@163.com>
2019-08-01 22:30:35 +0800

    第一次test git

---
+++ src/main/java/com/springtask/crontask.java
@@ -0,0 +1,14 @@
+package com.springtask;
+
+import java.util.Date;
+
+public class crontask {
+
+    public void excute1() {
+        System.out.println("excute1:" + new Date());
+    }
+
+    public void excute2() {
+        System.out.println("excute2:" + new Date());
+    }
+}
